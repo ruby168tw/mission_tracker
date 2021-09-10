e64ce1d5e3cddca280a323bc9539af7e4a92209b
Ruby <ruby008@dd-room.com>
2021-09-10 10:58:58 +0800

    regiter and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:success] = "註冊成功"
+          session[:user_id] = @user.id
+          @missions = Mission.find_by(id: @user.id)
+          redirect_to missions_path
+        else
+          flash[:error] = "註冊失敗"
+          render 'new'
+        end
+    end
+    
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :nickname)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_081727) do
+ActiveRecord::Schema.define(version: 2021_09_07_085446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_081727) do
   create_table "users", force: :cascade do |t|
     t.string "nickname"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "identity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
